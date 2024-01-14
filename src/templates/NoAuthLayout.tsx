f361eb5ea6f4ceb7a36728af26cb679bd8394bd8
KenIshii <riguveda44@gmail.com>
2024-01-14 23:44:07 +0900

    imageの大きさや表示崩れ修正

--- src/templates/NoAuthLayout.tsx
+++ src/templates/NoAuthLayout.tsx
@@ -27,8 +27,7 @@ export const NoAuthLayout: React.FC<Props> = ({ children }) => {
                 >
                     <div className="flex lg:flex-1">
                         <a href="/" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <Image className="h-8 w-auto" src={imageSrc} width={100} height={100} alt="" />
+                            <Image className="h-8 w-auto" src={imageSrc} width={32} height={32} alt="" />
                         </a>
                     </div>
                     <div className="flex lg:hidden">
@@ -37,7 +36,6 @@ export const NoAuthLayout: React.FC<Props> = ({ children }) => {
                             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                             onClick={() => setMobileMenuOpen(true)}
                         >
-                            <span className="sr-only">Open main menu</span>
                             <MenuIcon className="h-6 w-6" aria-hidden="true" />
                         </button>
                     </div>
